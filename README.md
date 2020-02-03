acf9ac7ba53136f02ea416c70e7e48c0e246fd23
CosaraG <kosharagen@gmail.com>
2020-02-03 11:05:02 +0100

    screenshot added in readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Pokemons List
+Pokemons List:
+img_src: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png,
+PokemonsList: https://pokeapi.co/api/v2/pokemon/?offset=0&limit=784,
 
 ![](./screenshot.jpg)
--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
 {
-    img: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png,
-    list: https://pokeapi.co/api/v2/pokemon/?offset=0&limit=784,
+    img_src: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png,
+    PokemonsList: https://pokeapi.co/api/v2/pokemon/?offset=0&limit=784,
 
 }
